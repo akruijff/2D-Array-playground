7e8258eef4324d5264528d1eac0f73ddae7f36df
Alex de Kruijff <alex.de.kruijff@mazarineblue.org>
2026-02-25 15:58:26 +0100

    Implement RandomIntGenerator

---
+++ 2D Array playground/RandomIntGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace _2D_Array_playground
+{
+    internal class RandomIntGenerator(int min, int max) : Fetcher<int>
+    {
+        private readonly int min = min, max = max;
+        private readonly Random random = new Random();
+
+        int Fetcher<int>.Fetch(int x, int y) => random.Next(min, max);
+    }
+}
