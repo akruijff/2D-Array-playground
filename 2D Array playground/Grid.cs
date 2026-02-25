9ce69f1c48f6027daa1bb4cdcd53dde35ec3e404
Alex de Kruijff <alex.de.kruijff@mazarineblue.org>
2026-02-25 15:43:42 +0100

    Introduce Grid(width, heigth)

---
+++ 2D Array playground/Grid.cs
@@ -0,0 +1,22 @@
+﻿namespace _2D_Array_playground
+{
+    internal class Grid<T>
+    {
+        private T[,] data;
+
+        public Grid(int width, int height)
+        {
+            data = new T[width, height];
+        }
+
+        public T Get(int x, int y)
+        {
+            return data[x, y];
+        }
+
+        public void Set(int x, int y, T value)
+        {
+            data[x, y] = value;
+        }
+    }
+}
